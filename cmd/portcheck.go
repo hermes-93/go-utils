70a6265978ffed8038df282b2a4865c59882ce91
Hermes <hermes-93@outlook.com>
2026-04-30 20:47:59 +0300

    feat: portcheck accepts multiple addresses

--- cmd/portcheck.go
+++ cmd/portcheck.go
@@ -12,20 +12,26 @@ func Portcheck(args []string) {
 	fs := flag.NewFlagSet("portcheck", flag.ExitOnError)
 	timeout := fs.Int("timeout", 5, "TCP dial timeout in seconds")
 	fs.Usage = func() {
-		fmt.Fprintln(os.Stderr, "Usage: devops portcheck [flags] <host:port>")
+		fmt.Fprintln(os.Stderr, "Usage: devops portcheck [flags] <host:port> [host:port...]")
 		fmt.Fprintln(os.Stderr, "\nFlags:")
 		fs.PrintDefaults()
 	}
 	fs.Parse(args) //nolint:errcheck
 
-	if fs.NArg() != 1 {
+	if fs.NArg() < 1 {
 		fs.Usage()
 		os.Exit(1)
 	}
 
-	result := port.Check(fs.Arg(0), *timeout)
-	fmt.Println(result.String())
-	if !result.Open {
+	failed := 0
+	for _, addr := range fs.Args() {
+		result := port.Check(addr, *timeout)
+		fmt.Println(result.String())
+		if !result.Open {
+			failed++
+		}
+	}
+	if failed > 0 {
 		os.Exit(1)
 	}
 }
